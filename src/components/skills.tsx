7bad26c2eae6a321a9f0c0cde8e8dde6acfd293e
teagueStockwell <tsAppDevelopment@gmail.com>
2021-07-20 16:06:07 -0700

    fix eslint

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-children-prop */
 import {v4} from 'uuid'
 import {SkillsCard} from './skills_card'
 import {Svgs} from './svgs'
@@ -46,7 +47,6 @@ const getSkills = (): {title: string; icons: JSX.Element[]}[] => {
 export const Skills = () => {
   return (
     <>
-      {/* eslint-disable-next-line react/no-children-prop */}
       {getSkills().map((s) => (
         <SkillsCard key={v4()} title={s.title} children={s.icons} />
       ))}
