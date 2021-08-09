ec31e4bdffde464f02ea1dc2fc1f4445d492a73d
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-08 19:15:55 -0700

    Init service worker

--- next.config.js
+++ next.config.js
@@ -1,44 +1,50 @@
 /* eslint-disable */
 const withPlugins = require('next-compose-plugins')
 const withAntdLess = require('next-plugin-antd-less')
+const withPWA = require('next-pwa')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
 
-module.exports = withPlugins(
-  [
-    [withBundleAnalyzer],
-    [
-      withAntdLess,
+const options = {
+  withBundleAnalyzer: {},
+
+  withAntDLess: {
+    modifyVars: {
+      '@primary-color': '#0d2',
+      '@border-radius-base': '10px',
+      '@border-color-base': '#ff00',
+      '@component-background': '#ff00',
+    },
+  },
+
+  withPwa: {
+    pwa: {
+      dest: 'public',
+    },
+    runtimeCaching: [
       {
-        modifyVars: {
-          '@primary-color': '#0d2',
-          '@border-radius-base': '10px',
-          '@border-color-base': '#ff00',
-          '@component-background': '#ff00',
+        urlPattern: /^https?.*/,
+        handler: 'StaleWhileRevalidate',
+        options: {
+          cacheName: 'offlineCache',
+          expiration: {
+            maxEntries: 200,
+          },
         },
       },
     ],
+  },
+}
+
+module.exports = withPlugins(
+  [
+    [withBundleAnalyzer, options.withBundleAnalyzer],
+    [withAntdLess, options.withAntDLess],
+    [withPWA, options.withPwa],
   ],
+
   {
     webpack5: true,
   }
 )
-
-// module.exports = withAntdLess({
-//   modifyVars: {
-//     '@primary-color': '#0d2',
-//     '@border-radius-base': '10px',
-//     '@border-color-base': '#ff00',
-//     '@component-background': '#ff00'
-//   },
-
-//   // Other NextConfig Here...
-//   webpack(config) {
-//     return config;
-//   },
-
-//   // NextFuture
-//   webpack5: true,
-
-// });
