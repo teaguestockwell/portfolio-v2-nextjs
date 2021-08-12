77ae182c95aff5716d897b2f78298a7389ae665b
teagueStockwell <tsAppDevelopment@gmail.com>
2021-08-12 13:38:45 -0700

    Init card fade in animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type {AppProps} from 'next/app'
 import 'video.js/dist/video-js.css'
 import '../style.css'
+import 'animate.css/animate.min.css'
 
 export default function App({Component, pageProps}: AppProps) {
   return (
