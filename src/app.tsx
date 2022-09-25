0cd270b2ee0a1597b5f8aad6530324715a286655
teagueStockwell <tsAppDevelopment@gmail.com>
2022-09-25 11:59:23 -0700

    Remove default seo

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,5 @@
 import {NextSeo} from 'next-seo'
 import React from 'react'
-import {portfolioData} from '../data/portfolio'
 import {DancingLinesWithTheme} from './components/dancing_lines'
 import {WithNav} from './components/nav/nav'
 import {AboutSection} from './components/sections/about'
@@ -20,11 +19,28 @@ export const App = ({
   portfolioContextValue: Types.PortfolioContext
 }) => {
   UseScrollToA()
+  const portfolioData = portfolioContextValue
 
   return (
     <>
       <NextSeo
         title={`${portfolioData.person.firstName} ${portfolioData.person.lastName} • Portfolio`}
+        openGraph={{
+          type: 'website',
+          locale: 'en_IE',
+          url: portfolioData.domain,
+          site_name: `${portfolioData.person.firstName} ${portfolioData.person.lastName} Portfolio`,
+          title: `${portfolioData.person.firstName} ${portfolioData.person.lastName} Portfolio`,
+          description: portfolioData.person.shortAbout,
+          images: [
+            {
+              url: `${portfolioData.domain}/heros/open-graph.png`,
+              height: 630,
+              width: 1200,
+              alt: portfolioData.domain,
+            },
+          ],
+        }}
       />
       <PortfolioContext.Provider value={portfolioContextValue}>
         <WithNav>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import type {AppProps} from 'next/app'
-import {DefaultSeo} from 'next-seo'
 import {ThemeProvider} from 'next-themes'
 import {portfolioData} from '../../data/portfolio'
 
@@ -31,24 +30,6 @@ export default function App({
       <meta
         name="viewport"
         content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover"
-      />
-      <DefaultSeo
-        openGraph={{
-          type: 'website',
-          locale: 'en_IE',
-          url: portfolioData.domain,
-          site_name: `${portfolioData.person.firstName} ${portfolioData.person.lastName} Portfolio`,
-          title: `${portfolioData.person.firstName} ${portfolioData.person.lastName} Portfolio`,
-          description: portfolioData.person.shortAbout,
-          images: [
-            {
-              url: `${portfolioData.domain}/heros/open-graph.png`,
-              height: 630,
-              width: 1200,
-              alt: portfolioData.domain,
-            },
-          ],
-        }}
       />
       <ThemeProvider disableTransitionOnChange={true}>
         <Component {...pageProps} />
